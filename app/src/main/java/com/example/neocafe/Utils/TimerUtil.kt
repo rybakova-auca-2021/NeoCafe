ddc2d110b40f762cf9e9ad331ec814a7ed14c018
kamiloyy_ <rybakova_k@auca.kg>
2023-07-09 15:52:45 +0600

    setup timer for code confirmation

---
+++ app/src/main/java/com/example/neocafe/Utils/TimerUtil.kt
@@ -0,0 +1,43 @@
+package com.example.neocafe.Utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.CountDownTimer
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import com.example.neocafe.R
+
+class TimerUtil(
+    private val context: Context,
+    private val button: Button,
+    private val navController: NavController
+) {
+    private lateinit var timer: CountDownTimer
+    private var remainingSeconds: Long = 0
+
+    fun startTimer(duration: Long, interval: Long) {
+        timer = object : CountDownTimer(duration, interval) {
+            override fun onTick(millisUntilFinished: Long) {
+                remainingSeconds = millisUntilFinished / 1000
+                val buttonText = "Отправить повторно через $remainingSeconds сек."
+                button.isEnabled = false
+                button.text = buttonText
+            }
+
+            @SuppressLint("ResourceAsColor")
+            override fun onFinish() {
+                button.isEnabled = true
+                val enabledColor = ContextCompat.getColor(context, R.color.main)
+                button.setBackgroundColor(enabledColor)
+                button.text = "Отправить повторно"
+                remainingSeconds = 0
+            }
+        }
+        timer.start()
+    }
+
+    fun cancelTimer() {
+        timer.cancel()
+    }
+}
--- app/src/main/java/com/example/neocafe/view/registration/CodeConfirmationFragment.kt
+++ app/src/main/java/com/example/neocafe/view/registration/CodeConfirmationFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.neocafe.R
+import com.example.neocafe.Utils.TimerUtil
 import com.example.neocafe.databinding.FragmentCodeConfirmationBinding
 
 class CodeConfirmationFragment : Fragment() {
     private lateinit var binding: FragmentCodeConfirmationBinding
+    private lateinit var timerUtil: TimerUtil
+    private lateinit var button: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +27,14 @@ class CodeConfirmationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupNavigation()
+        button = binding.buttonSendAgain
+        timerUtil = TimerUtil(requireContext(), button, findNavController())
+        timerUtil.startTimer(60000, 1000)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        timerUtil.cancelTimer()
     }
 
     private fun setupNavigation() {
