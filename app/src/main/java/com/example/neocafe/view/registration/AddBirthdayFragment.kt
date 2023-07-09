f5b84e8d33e6fe247186220c93755eb04c72a44e
kamiloyy_ <rybakova_k@auca.kg>
2023-07-09 16:10:01 +0600

    setup date picker and navigation in add birthday fragment

--- app/src/main/java/com/example/neocafe/view/registration/AddBirthdayFragment.kt
+++ app/src/main/java/com/example/neocafe/view/registration/AddBirthdayFragment.kt
@@ -1,21 +1,75 @@
 package com.example.neocafe.view.registration
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.neocafe.R
 import com.example.neocafe.databinding.FragmentAddBirthdayBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddBirthdayFragment : Fragment() {
     private lateinit var binding: FragmentAddBirthdayBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View {
         binding = FragmentAddBirthdayBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupNavigation()
+        showPicker()
+    }
+
+    private fun showPicker() {
+        binding.etBirthday.setOnClickListener {
+            showDatePicker()
+        }
+    }
+    private fun setupNavigation() {
+        binding.btnBack.setOnClickListener {
+            findNavController().navigate(R.id.action_addBirthdayFragment_to_codeConfirmationFragment)
+        }
+        binding.skip.setOnClickListener {
+            // TODO
+        }
+        binding.buttonLogin.setOnClickListener {
+            // TODO
+        }
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, year, month, day ->
+                val selectedCalendar = Calendar.getInstance()
+                selectedCalendar.set(Calendar.YEAR, year)
+                selectedCalendar.set(Calendar.MONTH, month)
+                selectedCalendar.set(Calendar.DAY_OF_MONTH, day)
+
+                val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+                val formattedDate = dateFormat.format(selectedCalendar.time)
+
+                binding.etBirthday.setText(formattedDate)
+            },
+            currentYear,
+            currentMonth,
+            currentDay
+        )
+
+        datePickerDialog.show()
     }
 }
